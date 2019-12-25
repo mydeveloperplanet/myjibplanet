28d4b8a6d3ab5aec565e91880e5021ce21a21881
mydeveloperplanet <gunter@mydeveloperplanet.com>
2019-12-25 10:08:39 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myjibplanet/MyJibPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myjibplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJibPlanetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJibPlanetApplication.class, args);
+    }
+
+}
